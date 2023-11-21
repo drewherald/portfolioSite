c4c29e3684a569716d59cde66fa4d1565d29fde8
drewherald <drewherald9@gmail.com>
2023-11-21 17:07:40 -0500

    created header area

---
+++ pages/script.js
@@ -0,0 +1,11 @@
+
+setTimeout(() =>{
+    document.getElementById("animatedName").classList.remove('typeAnimation')
+    document.getElementById('unAnimated').classList.add('occupationType')
+    document.getElementById('blurb').classList.add('blurb')
+    document.getElementById('blurb').removeAttribute('id')
+}, 2300)
+
+setTimeout(() =>{
+    document.getElementById('unAnimated').removeAttribute('id')
+}, 6000)
