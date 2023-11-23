d632a9a3af7e6a5761f76390ed60c50a29383938
drewherald <drewherald9@gmail.com>
2023-11-23 13:24:15 -0500

    built scrolling transitions and added footer

--- pages/script.js
+++ pages/script.js
@@ -10,3 +10,23 @@ setTimeout(() =>{
 setTimeout(() =>{
     document.getElementById('unAnimated').removeAttribute('id')
 }, 6000)
+
+// description animations
+
+const description = document.querySelectorAll('.animate-via-scroll')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('animated-via-scroll')
+        }
+    })
+}, {
+    threshold: 0.5
+})
+
+for(let i =0; i<description.length; i++){
+    const el = description[i]
+
+    observer.observe(el)
+}
